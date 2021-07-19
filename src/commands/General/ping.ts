36113fd2a7e92f0127542197c2203e8d0cb110c7
Megatank58 <tanumegatank@gmail.com>
2021-07-19 16:34:55 +0530

    lint: lint all files

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -4,20 +4,20 @@ import { Message } from "discord.js";
  * Sends the ping of the bot to the user.
  */
 class Ping extends Command {
-    constructor(context: PieceContext) {
-        super(context, {
-            name: "ping",
-            aliases: ["beep"],
-            description: "Sends your ping",
-            detailedDescription: `The ping is the difference between the
+	constructor(context: PieceContext) {
+		super(context, {
+			name: "ping",
+			aliases: ["beep"],
+			description: "Sends your ping",
+			detailedDescription: `The ping is the difference between the
             timestamp of your message and the timestamp of the bot message`,
-        });
-    }
+		});
+	}
 
-    async run(message: Message) {
-        const botMessage = await message.reply({ content: "Calculating ping!" })
-        botMessage.edit({ content: `BOT:${Math.round(this.container.client.ws.ping)}ms\nAPI:${botMessage.createdTimestamp - message.createdTimestamp}ms` });
-    }
+	async run(message: Message): Promise<void> {
+		const botMessage = await message.reply({ content: "Calculating ping!" });
+		botMessage.edit({ content: `BOT:${Math.round(this.container.client.ws.ping)}ms\nAPI:${botMessage.createdTimestamp - message.createdTimestamp}ms` });
+	}
 }
 
 export default Ping;
--- src/listeners/onReady.ts
+++ src/listeners/onReady.ts
@@ -3,16 +3,16 @@ import { Listener, PieceContext } from "@sapphire/framework";
  * Logs when the bot is ready to start
  */
 class OnReady extends Listener {
-    constructor(context: PieceContext) {
-        super(context, {
-            name: "ready",
-            once: true,
-        });
-    }
+	constructor(context: PieceContext) {
+		super(context, {
+			name: "ready",
+			once: true,
+		});
+	}
 
-    async run() {
-        this.container.logger.info("Bot has started!")
-    }
+	run(): void {
+		this.container.logger.info("Bot has started!");
+	}
 }
 
 export default OnReady;
